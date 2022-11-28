f7c5dcda0f0e1c791c7497d2613524c789f76ecb
Ainur <aynurpovn@mail.ru>
2022-11-28 11:06:15 +0200

    example_004 с делением рандомных чисел

--- Example_004/Program.cs
+++ Example_004/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA=7;
-double numberB=5;
+﻿double numberA=new Random().Next(1,10); // от 1 до 10
+Console.WriteLine(numberA);
+double numberB=new Random().Next(1,10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA/numberB);
